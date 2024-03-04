eb3cef2bab3797a5a09825a97dba660d928dd95a
Musab Al Mahi <musabalmahi53@gmail.com>
2024-03-04 20:24:17 +0600

    still not capable to create token when user login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,31 +32,42 @@ export default function Home() {
     <main>
       <h1>Nextjs authentication JWT verify http cookie only</h1>
 
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <div>
-          <label htmlFor="username">Username:</label>
+      <form onSubmit={handleSubmit} className="max-w-sm mx-auto gap-4">
+        <div className="mb-5">
+          <label
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="username"
+          >
+            Username:
+          </label>
           <input
             type="text"
             id="username"
             name="username"
             required
-            className="border rounded border-black"
+            placeholder="username here"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
+        <div className="mb-5">
+          <label
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="password"
+          >
+            Password:
+          </label>
           <input
             type="password"
             id="password"
             name="password"
             required
-            className="border rounded border-black"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
 
         <button
           type="submit"
-          className="p-2 bg-orange-600 text-white w-fit rounded"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Submit
         </button>
