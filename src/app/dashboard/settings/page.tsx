ef05c188a0e32ff85ac0886d5b2c3dd252a67ec1
Musab Al Mahi <musabalmahi53@gmail.com>
2024-03-06 23:38:18 +0600

    exploring nextJs authentication to do just simple authentication

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -2,7 +2,7 @@ export default function SettingsPage() {
     // This page is protected
     return (
         <div>
-            <h1>Super Secret Settings</h1>
+            <h1>Super Secret Settings.....</h1>
         </div>
     );
 }
